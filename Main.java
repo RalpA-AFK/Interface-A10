8a966a36bbf3e5e2f17396020f66797eca8ddade
RalpA-AFK <ralpaa@farmingdale.edu>
2025-04-20 21:12:15 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Priest p1 = new Priest("Constentine","Rosary");
+		
+		
+		
+		p1.dialouge(p1.getName());
+		p1.Weapon(p1.getWeaponName());
+		
+		
+	}
+
+}
